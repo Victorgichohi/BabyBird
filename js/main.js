6cb4ecc53eb953542568deabba07197a8993a489
Victorgichohi <victorgichohi48@gmail.com>
2015-11-23 13:24:07 +0300

    main page,icons and names sketches completed

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// $(window).scroll(function() {
+//
+//     if ($(this).scrollTop() > 300){
+//
+//         $('header').addClass("sticky");
+//     }
+//     else{
+//         $('header').removeClass("sticky");
+//     }
+//       // console.log($(window).scrollTop());
+// });
