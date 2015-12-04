cc9e0e5a0b496ef7e7079654a1d6a4d3ddb555ae
Victorgichohi <victorgichohi48@gmail.com>
2015-12-05 00:28:11 +0300

    toogle maximize which just shows a div on click of a button

--- js/main.js
+++ js/main.js
@@ -9,29 +9,33 @@ $(function(){
 				//  var scroll = $(window).scrollTop();
 				//  console.log(scroll);
     });
+    $(".btn-minimize").click(function(){
+    $(this).toggleClass('btn-plus');
+    $(".widget-content").slideToggle();
+  });
 });
 // start of slide in
-jQuery(function($) {
-
-    $('a.panel').click(function() {
-        var $target = $($(this).attr('href')),
-            $other = $target.siblings('.active');
-
-        if (!$target.hasClass('active')) {
-            $other.each(function(index, self) {
-                var $this = $(this);
-                $this.removeClass('active').animate({
-                    left: $this.width()
-                }, 500);
-            });
-
-            $target.addClass('active').show().css({
-                left: -($target.width())
-            }).animate({
-                left: 0
-            }, 500);
-        }
-    });
-
-});
+// jQuery(function($) {
+//
+//     $('a.panel').click(function() {
+//         var $target = $($(this).attr('href')),
+//             $other = $target.siblings('.active');
+//
+//         if (!$target.hasClass('active')) {
+//             $other.each(function(index, self) {
+//                 var $this = $(this);
+//                 $this.removeClass('active').animate({
+//                     left: $this.width()
+//                 }, 500);
+//             });
+//
+//             $target.addClass('active').show().css({
+//                 left: -($target.width())
+//             }).animate({
+//                 left: 0
+//             }, 500);
+//         }
+//     });
+//
+// });
 // stop of slide in
