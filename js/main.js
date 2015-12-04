a6df1b8db50459264a7e2ab9d77e92bc9192707a
Victorgichohi <victorgichohi48@gmail.com>
2015-12-05 00:42:15 +0300

    toogle sliding i from right to left

--- js/main.js
+++ js/main.js
@@ -9,33 +9,16 @@ $(function(){
 				//  var scroll = $(window).scrollTop();
 				//  console.log(scroll);
     });
-    $(".btn-minimize").click(function(){
-    $(this).toggleClass('btn-plus');
-    $(".widget-content").slideToggle();
-  });
+
 });
 // start of slide in
-// jQuery(function($) {
-//
-//     $('a.panel').click(function() {
-//         var $target = $($(this).attr('href')),
-//             $other = $target.siblings('.active');
-//
-//         if (!$target.hasClass('active')) {
-//             $other.each(function(index, self) {
-//                 var $this = $(this);
-//                 $this.removeClass('active').animate({
-//                     left: $this.width()
-//                 }, 500);
-//             });
-//
-//             $target.addClass('active').show().css({
-//                 left: -($target.width())
-//             }).animate({
-//                 left: 0
-//             }, 500);
-//         }
-//     });
-//
-// });
+$(document).ready(function () {
+    $('.hidden').hide()
+});
+
+$('.imagesDiv-link').click(function () {
+    $('#imagesDiv').animate({
+        width: 'toggle'
+    }, "slow")
+});
 // stop of slide in
