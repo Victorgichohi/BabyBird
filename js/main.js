6f47719c9fdebe5f3e980a61e4de6bbc18a8e130
Victorgichohi <victorgichohi48@gmail.com>
2015-12-04 22:56:14 +0300

    slide in demos working perfectly:)

--- js/main.js
+++ js/main.js
@@ -10,3 +10,28 @@ $(function(){
 				//  console.log(scroll);
     });
 });
+// start of slide in
+jQuery(function($) {
+
+    $('a.panel').click(function() {
+        var $target = $($(this).attr('href')),
+            $other = $target.siblings('.active');
+
+        if (!$target.hasClass('active')) {
+            $other.each(function(index, self) {
+                var $this = $(this);
+                $this.removeClass('active').animate({
+                    left: $this.width()
+                }, 500);
+            });
+
+            $target.addClass('active').show().css({
+                left: -($target.width())
+            }).animate({
+                left: 0
+            }, 500);
+        }
+    });
+
+});
+// stop of slide in
