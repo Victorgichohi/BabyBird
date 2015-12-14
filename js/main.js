0d7211048fa107abc241a19bec548ba350c5a1e1
Victorgichohi <victorgichohi48@gmail.com>
2015-12-14 14:02:35 +0300

    added smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* global $ */
 $(function(){
     $(window).scroll(function() {
         if ($(this).scrollTop() >=670) {
@@ -40,3 +41,18 @@ $('.web3click').click(function () {
 });
 
 // stop of slide in
+// smooth scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
